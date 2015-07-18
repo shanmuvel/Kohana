a5445b10ed2031bd5c2fe1ba5a6428c2d8494963
Shanmugan <shanmugan@riverstonetech.com>
2015-07-18 19:34:21 +0530

    User API functions are created

--- application/classes/Controller/Api/User.php
+++ application/classes/Controller/Api/User.php
@@ -43,10 +43,11 @@ class Controller_Api_User extends Controller {
         return $response;
     }
     
+    
     /**
      * User Signup API function
      * @param type $data
-     * @return type Description
+     * @return type $response as array
      */
     public static function signup($data) {
         $date = new DateTime();
@@ -69,6 +70,7 @@ class Controller_Api_User extends Controller {
         return $response;
     }
     
+    
     /**
      * Save User Registration Code
      * @param type $user_id
@@ -82,17 +84,19 @@ class Controller_Api_User extends Controller {
         $user_registration_code->save();
         
         // Send registration code to user through the email
-        self::send_signup_email($email, $registration_code, $config_path->code_verification_url);
+        self::send_signup_email($email, $registration_code, $config_path->code_verification_url, $user_id);
     }
+    
     
     /**
      * Send Signup Email to resgistered user with registration code
      * @param type $email
      */
-    public static function send_signup_email($email, $registration_code, $url) {
+    public static function send_signup_email($email, $registration_code, $url, $user_id) {
         $message = View::factory('template/mail/signup')
                 ->bind('email', $email)
                 ->bind('registration_code', $registration_code)
+                ->bind('user_id', $user_id)
                 ->bind('url', $url);
         $mail = array(
             'subject' => 'Welcome to KRCFM',
@@ -102,6 +106,118 @@ class Controller_Api_User extends Controller {
         );
         
         Email::send('default', $mail['subject'], $mail['body'], $mail['from'], $mail['to'], 'text/html');
+    }
+    
+    
+    /**
+     * User Code Verification API function
+     * @param type $data
+     * @return type $response as array
+     */
+    public static function user_code_verification($data) {
+        try {
+            $user_registration_code = ORM::factory('UserRegistrationCode')
+                    ->where("user_id", "=", $data['user_id'])
+                    ->and_where("registration_code", "=", $data["code"])
+                    ->find();
+
+            if ($user_registration_code != NULL):
+                $user_registration_code->delete();
+                $response = array('success' => true, 'message' => "Success");
+            else:
+                $response = array('success' => false, 'message' => "Invalid Code");
+            endif;
+        } catch (ErrorException $ex) {
+            $response = array('success' => false, 'message' => "Invalid Code");
+        }
+        return $response;
+    }
+    
+    /**
+     * User Profile creation API function
+     * @param type $data
+     * @return type $response as array
+     */
+    public static function create_user_profile($data) {
+
+        try {
+            // Update User Details
+            $user = ORM::factory('User', $data['user_id']);
+            $user->password = $data['password'];
+            $user->save();
+
+            $user_info = ORM::factory('UserInfo')->where('user_id', '=', $data['user_id'])->count_all();
+            if ($user_info > 0):
+                self::update_profile_info($data);
+                $response = array('success' => true, 'message' => "Profile was successfully updated");
+            else:
+                self::create_profile_info($data);
+                $response = array('success' => true, 'message' => "Success!! Profile was successfully created and activation link has been sent your mail.");
+            endif;
+        } catch (ErrorException $ex) {
+            $response = array('success' => false, 'message' => "Sorry, Invalid Request or internal problem");
+        }
+
+        return $response;
+    }
+
+    /**
+     * Create New Profile Info
+     * @param type $user_id
+     */
+    public static function create_profile_info($data) {
+        $config_path = Kohana::$config->load('myconf');
+        $date = new DateTime();
+        $user_info = ORM::factory('UserInfo');
+        $user_info->user_id = $data['user_id'];
+        $user_info->first_name = $data['firstname'];
+        $user_info->last_name = $data['lastname'];
+        $user_info->company_name = $data['company'];
+        $user_info->telephone_number = $data['phoneno'];
+        $user_info->created_at = $date->format('Y-m-d H:i:s');
+        $user_info->updated_at = $date->format('Y-m-d H:i:s');
+        $user_info->save();
+        
+        // Send profile activation email to user
+        self::send_profile_activation_email($config_path->profile_activation_url, $data['user_id'], $data['firstname']);
+    }
+    
+     /**
+     * Send Profile activation email to registered user
+     * @param type $email
+     */
+    public static function send_profile_activation_email($url, $user_id, $name) {
+        $message = View::factory('template/mail/profile_activation')
+                ->bind('name', $name)
+                ->bind('user_id', $user_id)
+                ->bind('url', $url);
+        $mail = array(
+            'subject' => 'KRCFM-Profile Activation',
+            'body' => $message,
+            'from' => array('admin@myschedule.org' => 'KRCFM'),
+            'to' => "shanmu.grs24@gmail.com"
+        );
+
+        Email::send('default', $mail['subject'], $mail['body'], $mail['from'], $mail['to'], 'text/html');
+    }
+    
+     /**
+     * Create New Profile Info
+     * @param type $user_id
+     */
+    public static function update_profile_info($data) {
+        $date = new DateTime();
+        $user_info = ORM::factory('UserInfo')->where('user_id', '=', $data['user_id'])->find();
+        $user_info->first_name = $data['firstname'];
+        $user_info->last_name = $data['lastname'];
+        $user_info->company_name = $data['company'];
+        $user_info->telephone_number = $data['phoneno'];
+        $user_info->updated_at = $date->format('Y-m-d H:i:s');
+        $user_info->save();
+    }
+    
+    public static function get_user_info($data) {
+        
     }
 
 }
