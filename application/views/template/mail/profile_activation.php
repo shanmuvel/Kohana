708ece5a206b79a58cac3aa75f99f48c056155bf
Shanmugan <shanmugan@riverstonetech.com>
2015-07-18 19:36:48 +0530

    Create a new mail template for Profile activation

---
+++ application/views/template/mail/profile_activation.php
@@ -0,0 +1,11 @@
+<div>
+    <div>
+    <?php $base_url = URL::site(NULL, TRUE);
+    ?>
+    </div>
+    <p>Hi <?= ucfirst($name); ?>,</p>
+    <p>Your profile was successfully created in KRCFM<p>
+    <p>Please click the below link to activate your account</p>
+    <p><a href="<?= $url; ?>?user_id=<?= $user_id; ?>"><?= $url; ?>?user_id=<?= $user_id; ?></a></p>
+    <p>Regards, <br />KRCFM Team</p>
+</div>
